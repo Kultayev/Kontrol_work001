631a96f146dc210c84113d92d05b3d181db8f6b8
Serik <seke@bk.ru>
2022-07-22 00:08:08 +0600

    Решил задачу с использованием foreach

--- Program.cs
+++ Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World33333333!");
+﻿// Сформировать массив строк, далее вывести на экран массив, где в словах три и менее символов
+using System;
+Console.Write("Вот наш массив :\t ");
+string [] myArray = {"Hello", "bro", "how", "do", "you", "do?"};
+//string [] myArray2 = {};
+string [] myArray2 = new string[myArray.Length];
+for (int k = 0; k <= 5; k++)
+    {
+        
+        Console.Write(" " + myArray[k]); // вывод первоначального массива через пробел
+        //if (value.Length <=3)
+        //myArray2[i] = value;
+    }
+    var i=0;
+    foreach (var value in myArray)
+    {
+        if (value.Length <=3)
+        {
+            myArray2[i] = value; i++;
+        }
+    }
+    Console.WriteLine();
+Console.Write("Массив с элементами от 3-х и менее символов:\t");
+    for (int j = 0; j <= 5; j++)
+    {
+        
+        Console.Write(" " + myArray2[j]); // вывод массива через пробел min 3 letters
+        
+            }
